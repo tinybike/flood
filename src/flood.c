82942eaf34b23e1c6c3a1cd580ae426c973cb5b9
Jack Peterson <jack@tinybike.net>
2014-12-29 23:02:38 -0800

    removed gethostbyname call

--- src/flood.c
+++ src/flood.c
@@ -399,15 +399,15 @@ void share(const char *ip)
     /* broadcast links */
     debug(" - Link broadcast\n");
 
-    if ( (he = gethostbyname(ip)) == NULL) die("[share] gethostbyname");
+    // if ( (he = gethostbyname(ip)) == NULL) die("[share] gethostbyname");
     // if ( (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast) == -1))
     //     die("[share] setsockopt(SO_BROADCAST)");
 
-    their_addr.sin_family = AF_INET;
-    their_addr.sin_port = htons(PORT);
-    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    // their_addr.sin_family = AF_INET;
+    // their_addr.sin_port = htons(PORT);
+    // their_addr.sin_addr = *((struct in_addr *)he->h_addr);
 
-    memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
+    memset(xtrnaddr.sin_zero, '\0', sizeof xtrnaddr.sin_zero);
 
     leveldb_iter_seek_to_first(iter);
     while (leveldb_iter_valid(iter))
@@ -420,9 +420,9 @@ void share(const char *ip)
         strlcpy(buf, link, BUFLEN);
         bufptr = (char *)&buf;
         while (remain > 0) {
-            rc = sendto(sockfd, bufptr, remain, 0, (struct sockaddr *)&their_addr, sizeof their_addr);
+            rc = sendto(sockfd, bufptr, remain, 0, (struct sockaddr *)&xtrnaddr, sizeof xtrnaddr);
             if (rc == -1) die("[share] sendto");
-            debug(" - Sent %s to %s [%d bytes]\n", hash, inet_ntoa(their_addr.sin_addr), rc);
+            debug(" - Sent %s to %s [%d bytes]\n", hash, inet_ntoa(xtrnaddr.sin_addr), rc);
             remain -= rc;
             bufptr += rc;
         }
